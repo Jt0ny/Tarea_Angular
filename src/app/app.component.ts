c61db02225b332d587e5ea74d72399929e94f4d8
Jt0ny <jtonytipan@gmail.com>
2024-12-03 22:29:39 -0500

    Tarea Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Tarea_Angular';
-}
+}*/
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', // Este es el selector que se usa en el index.html
+  template: `<h1>Hola Mundo con Angular</h1>`, // Aquí va el contenido directamente
+  styles: []
+})
+export class AppComponent {}
+
